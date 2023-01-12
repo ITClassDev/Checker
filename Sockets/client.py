cfe7af06dc19d7df707fd7a4106a57e094e79f95
Stepan Andreichev <85014298+StepWolf45@users.noreply.github.com>
2023-01-13 00:28:45 +0300

    Delete client.py

--- Sockets/client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 8888))
-client.sendall(b"Hello, world!")
-data = client.recv(2000)
-print(data)
