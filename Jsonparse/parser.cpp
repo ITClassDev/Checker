64581756d20b2d6ac289bcf9b446caf918cabfd2
Stepan Andreichev <85014298+StepWolf45@users.noreply.github.com>
2023-01-14 10:16:23 +0300

    Add files via upload

--- Jsonparse/parser.cpp
+++ Jsonparse/parser.cpp
@@ -1,26 +1,43 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstdio>
 #include "json.hpp"
+
 using json = nlohmann::json;
 using namespace std;
 
-std::string slurp(std::ifstream& in) {
-    std::ostringstream sstr;
-    sstr << in.rdbuf();
-    return sstr.str();
-}
-
 int main() {
-    ifstream file("tests.json");
-    string content( (std::istreambuf_iterator<char>(file) ),(std::istreambuf_iterator<char>()    ) );
-    cout<<content;
-    json responseJson = json::parse(content);
-    cout<<responseJson;
-    json object = responseJson["response"];
-    string first_name = object[0]["first_name"];
-    string last_name = object[0]["last_name"];
-    cout << first_name << endl;
-    cout << last_name << endl;
-    return 0;
+    long lSize;
+    char * buffer;
+    size_t result;
+    FILE * file;
+
+    file = fopen("../Local/tests.json", "r");
+
+    fseek(file, 0, SEEK_END);
+    lSize = ftell(file);
+    rewind(file);
+    buffer = (char*) malloc (sizeof(char)*lSize);
+    if(buffer == nullptr){
+        fputs("Memory error", stderr);
+        exit(2);
+    }
+    result = fread(buffer,1,lSize,file);
+    if(result != lSize){
+        fputs("Reading error", stderr);
+        exit(3);
+    }
+
+    json data = json::parse(buffer);
+    json object = data["tests"];
+    for (auto& [key, value] : object.items()) {
+        cout << value["input"] <<value["output"]<< "\n";
+    }
+    fclose(file);
+    free(buffer);
+    return  0;
+
+
+
 }
